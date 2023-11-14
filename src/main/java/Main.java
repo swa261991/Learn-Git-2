878ffb99033061ca93f75f82674c48adb3685a28
YoYo <xyz@gmail.com>
2023-11-14 16:29:14 +0530

    feature1-commit-3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("master-commit-2");
         System.out.println("feature1-commit-1");
         System.out.println("feature1-commit-2");
+        System.out.println("feature1-commit-3");
     }
 }
