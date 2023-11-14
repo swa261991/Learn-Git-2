87e97717e35758a981e6552fc72f200f9c9a3e74
YoYo <xyz@gmail.com>
2023-11-14 16:28:46 +0530

    master-commit-2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("master-commit-1");
+        System.out.println("master-commit-2");
         System.out.println("feature1-commit-1");
         System.out.println("feature1-commit-2");
         System.out.println("feature1-commit-3");
